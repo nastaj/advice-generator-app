02b3d4473f82b2be0d96616096f26b00ec2a6ccb
nastaj <G00424689@atu.ie>
2023-07-12 15:55:54 +0200

    implemented main functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// HTML Elements
+const adviceIdEl = document.querySelector(".advice-id");
+const adviceEl = document.querySelector(".advice");
+const generateBtn = document.querySelector(".generate-btn");
+
+// App
+class App {
+  constructor() {
+    generateBtn.addEventListener("click", this._renderAdvice.bind(this));
+  }
+
+  // Fetch random advice from API
+  async _getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    return data;
+  }
+
+  // Render received slip in DOM
+  async _renderAdvice() {
+    const { slip } = await this._getAdvice();
+
+    adviceIdEl.textContent = slip.id;
+    adviceEl.textContent = `"${slip.advice}"`;
+  }
+}
+
+const app = new App();
